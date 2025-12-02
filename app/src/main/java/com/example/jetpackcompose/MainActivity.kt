fcd2497feffb13847fb429828283db67f55cc570
Himanshu <himanshuraj48512@gmail.com>
2025-12-02 13:28:35 +0530

    Toggle in android

---
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicSwitchExample()
+        }
+    }
+}
+
+@Composable
+fun BasicSwitchExample() {
+    var isChecked by remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(text = if (isChecked) "ON" else "OFF")
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Switch(
+            checked = isChecked,
+            onCheckedChange = { isChecked = it }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSwitch() {
+    BasicSwitchExample()
+}
