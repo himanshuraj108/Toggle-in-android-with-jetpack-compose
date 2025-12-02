040ec90e3dfcd4c19779f3ec98a6274346d7aec7
Himanshu <himanshuraj48512@gmail.com>
2025-12-02 13:40:34 +0530

    Some color operation perform using some conditions

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -23,7 +25,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicSwitchExample()
+            PreviewSwitch()
         }
     }
 }
@@ -36,12 +38,21 @@ fun BasicSwitchExample() {
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.padding(16.dp)
     ) {
-        Text(text = if (isChecked) "ON" else "OFF")
+        Text(text = if (isChecked) "ON" else "OFF",
+            color = if(isChecked) Color.Blue else Color.Red
+            )
         Spacer(modifier = Modifier.width(8.dp))
 
         Switch(
             checked = isChecked,
-            onCheckedChange = { isChecked = it }
+            onCheckedChange = { isChecked = it },
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = if(isChecked) Color.Blue else Color.Red,
+                checkedTrackColor = Color(0xFFCB1616),
+                uncheckedThumbColor = Color.Red,
+                uncheckedTrackColor = if(isChecked) Color.Blue else Color.Red
+
+            )
         )
     }
 }
